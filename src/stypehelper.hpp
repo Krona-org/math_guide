2341cead9ff4e4671c24fa6218e5991f9f9a463e
Krona-org <krona.original@gmail.com>
2026-07-01 17:40:30 +0300

    добавил стиль прозрачности getGlassStyle

--- src/stypehelper.hpp
+++ src/stypehelper.hpp
@@ -4,14 +4,34 @@
 class StyleHelper
 {
 public:
-    static inline QString getMainWidgetStyle();
+    static inline QString getMainWidgetStyle(const QString &imagePath);
+    static inline QString getGlassStyle();
 };
 
+
 // Стиль для главного виджета
-QString StyleHelper::getMainWidgetStyle() {
-    return R"(
+QString StyleHelper::getMainWidgetStyle(const QString &imagePath) {
+    return QString(R"(
         QWidget#centralwidget {
-            border-image: url(:/res/image/kot_v_vode.png) 0 0 0 0 stretch stretch;
+            border-image: url(%1) 0 0 0 0 stretch stretch;
+        }
+    )").arg(imagePath);
+}
+
+QString StyleHelper::getGlassStyle(){
+    return R"( 
+        QWidget, QGroupBox {
+          background-color: rgba(255, 255, 255, 20);   /* полупрозрачный белый */
+          border: 1px solid rgba(255, 255, 255, 40);   /* полупрозрачная рамка */
+          border-radius: 12px;                         /* скруглённые углы */
+          color: white;
+          margin-top: 8px;
+        }
+        QGroupBox::title {
+          subcontrol-origin: margin;
+          left: 10px;
+          color: rgba(255, 255, 255, 200);
         }
     )";
+
 }
